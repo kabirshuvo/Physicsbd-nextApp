0e2c29e5246156dfc3288e12bce8c03a7bcc7650
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-08 11:49:04 +0600

    Development branch created and initial project setup done

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div>
+      <h3 className='text-center text-3xl'>Signup Page</h3>
+    </div>
+  )
+}
+
+export default SignupPage
