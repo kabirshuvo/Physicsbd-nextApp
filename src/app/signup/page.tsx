403497f00b54b0a3ed1396446b9cf991351d4933
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-08 17:29:13 +0600

    Got an error

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+import React from "react";
 
 const SignupPage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
   return (
     <div>
-      <h3 className='text-center text-3xl'>Signup Page</h3>
+      <h3 className="flex flex-col justify-center items-center text-center text-3xl min-h-screen">
+        Signup Page
+      </h3>
+      <br />
+      <label htmlFor="username">username</label>
+      <input
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
